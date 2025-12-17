271f0c431b2942e535b56220bc7db2529a94d53a
SeshuTarapatla <seshu.tarapatla@gmail.com>
2025-12-17 12:08:53 +0530

    GenderClassifier class

---
+++ src/gender_classify_llm/__init__.py
@@ -0,0 +1,64 @@
+__version__ = "0.1.0"
+
+from json import loads
+from pathlib import Path
+from typing import Literal, TypedDict
+
+import ollama
+
+
+SYSTEM_PROMPT = """
+You are a strict binary classification system.
+
+Task:
+Estimate the most likely gender using the provided profile image and text.
+This is a probabilistic inference, not ground truth.
+
+Output rules (MANDATORY):
+- Output MUST be valid JSON
+- Output MUST contain ONLY these keys:
+  - gender
+  - confidence
+  - reason
+- gender MUST be an string with the below possible values only:
+  - female
+  - male
+- confidence MUST be a float between 0 and 1
+- reason MUST be a string with a single line describing:
+  - why do you think it is that gender
+- No explanations
+- No extra keys
+- No text outside JSON
+- No markdown
+- No comments
+
+Behavior rules:
+- Treat this as a classification task, not a reasoning task
+- Do NOT perform or reveal step-by-step reasoning
+- Minimize internal deliberation
+- Respond decisively within constraints
+- Never ask questions
+- Never apologize
+- If output violates rules, immediately correct and output valid JSON only
+- Be as quick as possible to deliver the output
+"""
+
+class Prediction(TypedDict):
+    gender: Literal["male", "female"]
+    confidence: float
+    reason: str
+
+class GenderClassifier:
+    def __init__(self, model: str = "qwen3-vl:4b-instruct") -> None:
+        self.model = model
+        self.__system_prompt__ = SYSTEM_PROMPT
+
+    def predict(self, image: Path | str) -> Prediction:
+        response = ollama.generate(
+            model=self.model,
+            system=self.__system_prompt__,
+            images = [str(image)],
+            prompt="Predict the gender from the image provided."
+        ).response
+        result: Prediction = loads(response)
+        return result
