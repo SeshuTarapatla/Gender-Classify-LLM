ce63e70c7d5cced1f4629b9cff447d615ee307dd
SeshuTarapatla <seshu.tarapatla@gmail.com>
2025-12-17 12:15:37 +0530

    docs: enhance docstrings for Prediction and GenderClassifier classes

--- src/gender_classify_llm/__init__.py
+++ src/gender_classify_llm/__init__.py
@@ -7,6 +7,7 @@ from typing import Literal, TypedDict
 import ollama
 
 
+# Defining the system prompt for the gender classification task
 SYSTEM_PROMPT = """
 You are a strict binary classification system.
 
@@ -43,22 +44,49 @@ Behavior rules:
 - Be as quick as possible to deliver the output
 """
 
+
 class Prediction(TypedDict):
+    """
+    Represents the prediction result from the gender classification request.
+
+    Attributes:
+        gender (Literal["male", "female"]): The predicted gender of the input.
+        confidence (float): The confidence level of the prediction, a value between 0 and 1.
+        reason (str): A textual explanation or reasoning for the prediction.
+    """
+
     gender: Literal["male", "female"]
     confidence: float
     reason: str
 
+
 class GenderClassifier:
+    """
+    A classifier for determining gender from an image using local VLM.
+    """
+
     def __init__(self, model: str = "qwen3-vl:4b-instruct") -> None:
+        """
+        Initialize the GenderClassifyLLM model with a specified model name.
+        Args:
+            model (str, optional): The name of the model to use. Defaults to "**qwen3-vl:4b-instruct**".
+        """
+
         self.model = model
         self.__system_prompt__ = SYSTEM_PROMPT
 
     def predict(self, image: Path | str) -> Prediction:
+        """Predict the gender of a person in an image using an LLM model.
+        Args:
+            image (Path | str): Path to the image file or image string.
+        Returns:
+            Prediction: A Prediction object containing the predicted gender, confidence, and reason.
+        """
         response = ollama.generate(
             model=self.model,
             system=self.__system_prompt__,
-            images = [str(image)],
-            prompt="Predict the gender from the image provided."
+            images=[str(image)],
+            prompt="Predict the gender from the image provided.",
         ).response
         result: Prediction = loads(response)
         return result
