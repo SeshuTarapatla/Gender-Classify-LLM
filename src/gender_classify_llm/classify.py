69dafe65daae8aa3a9c48260bfa15bfc77f7ce12
SeshuTarapatla <seshu.tarapatla@gmail.com>
2025-12-18 18:06:24 +0530

    Refactor gender classification model with detailed docstrings and improved error handling
    - Improved error handling in `ollama.py` by adding a new method `is_running`
    - Updated class and method documentation to include descriptions and parameter details

--- src/gender_classify_llm/classify.py
+++ src/gender_classify_llm/classify.py
@@ -7,6 +7,7 @@ from rich.status import Status
 from .ollama import ollama
 
 
+# Defining the system prompt for the LLM for gender classification.
 SYSTEM_PROMPT = """
 You are a strict binary classification system.
 
@@ -45,21 +46,55 @@ Behavior rules:
 
 
 class Prediction(TypedDict):
+    """
+    Represents the prediction result from a gender classifier.
+
+    Attributes:
+        gender (Literal["male", "female"]): The predicted gender of the subject.
+        confidence (float): The model's confidence score (0.0 to 1.0).
+        reason (str): A textual explanation or reasoning for the prediction.
+    """
+
     gender: Literal["male", "female"]
     confidence: float
     reason: str
 
 
 class GenderClassifier:
+    """
+    A class for classifying gender using a ollama vision language model.
+    This class provides methods to process image & text and predict gender based on visual and  linguistic patterns.
+    """
+
     def __init__(self, model: str = "qwen3-vl:4b-instruct") -> None:
-        
+        """
+        Initializes the GenderClassifier with an Ollama model.
+
+        Args:
+            model (str): The Ollama model name to use. Defaults to "**qwen3-vl:4b-instruct**".
+
+        Raises:
+            Exception: If the model or ollama does not exist.
+        """
+
         self.model = model
         self.__system_prompt__ = SYSTEM_PROMPT
         ollama.exists(self.model)
-        with Status(f"Loading the model [green]{self.model}[/]..."):
-            self._generate("Hi")
+        if not ollama.is_running(self.model):
+            with Status(f"Loading the model [green]{self.model}[/]..."):
+                self._generate("Hi")
 
     def _generate(self, prompt: str, image: str | Path | None = None) -> str:
+        """
+        Generates a response using the Ollama model.
+
+        Args:
+            prompt (str): The input text prompt.
+            image (str | Path | None, optional): The path to an image file. Defaults to None.
+
+        Returns:
+            str: The generated response as a string.
+        """
         return ollama.generate(
             model=self.model,
             system=self.__system_prompt__,
@@ -68,6 +103,14 @@ class GenderClassifier:
         ).response
 
     def predict(self, image: str | Path) -> Prediction:
+        """
+        Predicts the gender of a subject based on given image.
+
+        Args:
+            image (str | Path): The path to the image file.
+        Returns:
+            Prediction: A dictionary containing the predicted gender, confidence score, and reason.
+        """
         response = self._generate(
             prompt="Predict the gender from the image provided.", image=image
         )
